c143c3ed8064d423f2ab07be58c125058d7d986c
Mykola Hutorov <Mykola_Hutorov@epam.com>
2022-06-13 22:41:51 +0300

    Add OrderSubmitFunctions project and OrderItemsBlobResolver with BlobContainerService

---
+++ src/OrderSubmitFunctions/OrderItemsBlobResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using OrderSubmitFunctions.Services;
+
+namespace OrderSubmitFunctions;
+
+public static class OrderItemsBlobResolver
+{
+    private static string ConnectionString => Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+    private static string ContainerName => Environment.GetEnvironmentVariable("OrdersContainerName");
+
+    [FunctionName("OrderItemsBlobResolver")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Admin, "post", Route = null)] HttpRequest req,
+        ILogger logger)
+    {
+        try
+        {
+            logger.LogInformation($"[OrderItemsBlobResolver.Run] Trying to process order json to the blob container: {ContainerName} with connection string: {ConnectionString}...");
+
+            var blobService = new BlobContainerService(ConnectionString, ContainerName, logger);
+            var fileName = await blobService.Upload(req.Body);
+            
+            return new OkObjectResult($"The file: {fileName} is loaded successfully to the container: {ContainerName}");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"[OrderItemsBlobResolver.Run] Error: {ex.Message}");
+            return new NoContentResult();
+        }
+    }
+}
---
+++ src/OrderSubmitFunctions/Services/BlobContainerService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Logging;
+
+namespace OrderSubmitFunctions.Services;
+
+internal class BlobContainerService
+{
+    private readonly BlobServiceClient serviceClient;
+    private readonly BlobContainerClient containerClient;
+    
+    private readonly ILogger logger;
+
+    public BlobContainerService(
+        string connectionString, 
+        string containerName,
+        ILogger logger,
+        int maxRetriesCount = 3
+    )
+    {
+        this.logger = logger;
+
+        this.serviceClient = GetBlobServiceClient(connectionString, maxRetriesCount);
+        this.containerClient = GetBlobContainerClient(containerName);
+    }
+
+    public BlobServiceClient GetBlobServiceClient(string connectionString, int maxRetriesCount)
+    {
+        this.logger.LogDebug($"[BlobContainerService] Creating service client...");
+        
+        var blobClientOptions = new BlobClientOptions();
+        blobClientOptions.Retry.MaxRetries = maxRetriesCount;
+        
+        return new BlobServiceClient(connectionString, blobClientOptions);
+    }
+
+    public BlobContainerClient GetBlobContainerClient(string containerName)
+    {
+        this.logger.LogDebug($"[BlobContainerService] Creating container client...");
+        return serviceClient.GetBlobContainerClient(containerName);
+    }
+
+    public async Task<string> Upload(Stream stream)
+    {
+        var fileName = DateTime.UtcNow.ToString("O");
+
+        this.logger.LogDebug($"[BlobContainerService.Upload] Trying to upload order file: {fileName} from a stream...");
+        await this.Upload(stream, fileName);
+
+        return fileName;
+    }
+
+    public async Task Upload(Stream stream, string fileName)
+    {
+        var streamContent = await new StreamReader(stream).ReadToEndAsync();        
+        await this.Upload(streamContent, fileName);
+    }
+
+    public async Task<string> Upload(string text)
+    {
+        var fileName = DateTime.UtcNow.ToString("O");
+
+        this.logger.LogDebug($"[BlobContainerService.Upload] Trying to upload order file: {fileName} from a string...");
+        await this.Upload(text, fileName);
+
+        return fileName;
+    }
+
+    public async Task Upload(string text, string fileName)
+    {
+        var encodedBytes = Encoding.UTF8.GetBytes(text);
+        await this.Upload(encodedBytes, fileName);
+    }
+
+    public async Task Upload(byte[] bytes, string fileName)
+    {
+        using (var memoryStream = new MemoryStream(bytes))
+        {
+            await this.Upload(memoryStream, fileName);
+        }
+    }
+
+    public async Task Upload(MemoryStream memoryStream, string fileName)
+    {
+        await this.containerClient.CreateIfNotExistsAsync();
+        var blobClient = this.containerClient.GetBlobClient(fileName);       
+        await blobClient.UploadAsync(memoryStream);
+    }
+}
