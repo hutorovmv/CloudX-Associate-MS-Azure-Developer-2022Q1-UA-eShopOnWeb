a695eb4d4a2feba2f20bf127a2ed03b26e78b590
Mykola Hutorov <Mykola_Hutorov@epam.com>
2022-06-13 22:44:05 +0300

    Add OrderItemsResolver and ServiceBusNotificationService

---
+++ src/OrderSubmitFunctions/OrderItemsResolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using OrderSubmitFunctions.Services;
+
+namespace OrderSubmitFunctions;
+
+public class OrderItemsResolver
+{
+    private readonly ILogger<OrderItemsResolver> logger;
+
+    private string ConnectionString => Environment.GetEnvironmentVariable("ServiceBusConnectionString");
+
+    private static string ContainerName => Environment.GetEnvironmentVariable("OrdersContainerName");
+
+    private static string TopicName => Environment.GetEnvironmentVariable("OrdersTopicName");
+
+    private static string OrderBlobUploadFailureSubject => Environment.GetEnvironmentVariable("OrderBlobUploadFailureSubject");
+
+    public OrderItemsResolver(ILogger<OrderItemsResolver> logger)
+    {
+        this.logger = logger;
+    }
+
+    [FunctionName("OrderItemsResolver")]
+    public async Task Run([ServiceBusTrigger("orders", "order-upload", Connection = "ServiceBusConnectionString")]string message)
+    {
+        try
+        {
+            this.logger.LogInformation($"[OrderItemsResolver.Run] Trying to process order json to the blob container: {ContainerName} with connection string: {ConnectionString}...");
+
+            // Error to test fallback Logic App
+            // throw new Exception();
+
+            var blobService = new BlobContainerService(ConnectionString, ContainerName, logger);
+            var fileName = await blobService.Upload(message);
+
+            this.logger.LogInformation($"The file: {fileName} is loaded successfully to the container: {ContainerName}");
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError($"[OrderItemsResolver.Run] Error on blob upload: {ex.Message}");
+            var notificationService = new ServiceBusNotificationService(ConnectionString, TopicName, this.logger);
+            await notificationService.SendMessage(message, OrderBlobUploadFailureSubject);
+        }
+    }
+}
---
+++ src/OrderSubmitFunctions/Services/ServiceBusNotificationService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Logging;
+
+namespace OrderSubmitFunctions.Services;
+
+internal class ServiceBusNotificationService : IDisposable
+{
+    private readonly ServiceBusClient client;
+    private readonly ServiceBusSender sender;
+    private readonly ILogger logger;
+
+    public ServiceBusNotificationService(
+        string connectionString, 
+        string topicName,
+        ILogger logger
+    )
+    {
+        this.logger = logger;
+
+        this.logger.LogDebug($"[ServiceBusNotificationService] Trying to initialize service bus client with connection string: {connectionString}...");
+        this.client = new ServiceBusClient(connectionString);
+
+        this.logger.LogDebug($"[ServiceBusNotificationService] Trying to initialize service bus sender with topic: {topicName}...");
+        this.sender = this.client.CreateSender(topicName);
+    }
+
+    public async Task SendMessage(string message, string subject)
+    {
+        this.logger.LogDebug($"[ServiceBusNotificationService.SendMessage] Trying to send message: {message} with subject: {subject}...");
+
+        await this.sender.SendMessageAsync(new ServiceBusMessage(message)
+        {
+            Subject = subject
+        });
+    }
+
+    public async void Dispose()
+    {
+        await this.client.DisposeAsync();
+    }
+}
